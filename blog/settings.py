0ffc062b7d512fac1cb3918ee99f3906811de7a3
Yahor Shumski <yahor.shumski@gmail.com>
2015-03-22 18:32:07 +0000

    Implemented pagination

--- blog/settings.py
+++ blog/settings.py
@@ -87,7 +87,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = 'blogengine/static/'
+STATIC_URL = '/static/'
 
 # Template directory
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
