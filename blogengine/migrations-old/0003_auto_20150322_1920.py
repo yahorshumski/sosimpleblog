d08c8617b80ced2666a8616f6e5a7442a6a7e3e8
Yahor Shumski <yahor.shumski@gmail.com>
2015-03-23 19:46:47 +0000

    Implemented flat page support

---
+++ blogengine/migrations-old/0003_auto_20150322_1920.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blogengine', '0002_auto_20150322_1841'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='pub_date',
+            field=models.DateTimeField(auto_now_add=True),
+            preserve_default=True,
+        ),
+    ]
