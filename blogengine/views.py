3b537fd599e06f03c61da11646da14b0c5510cd1
Yahor Shumski <yahor.shumski@gmail.com>
2015-03-28 19:33:55 +0000

    Tags are now shown

--- blogengine/views.py
+++ blogengine/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from blogengine.models import Category, Post
+from blogengine.models import Category, Post, Tag
 
 
 # Create your views here.
@@ -12,4 +12,13 @@ class CategoryListView(ListView):
             category = Category.objects.get(slug=slug)
             return Post.objects.filter(category=category)
         except Category.DoesNotExist:
+            return Post.objects.none()
+
+class TagListView(ListView):
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+        try:
+            tag = Tag.objects.get(slug=slug)
+            return tag.post_set.all()
+        except Tag.DoesNotExist:
             return Post.objects.none()
